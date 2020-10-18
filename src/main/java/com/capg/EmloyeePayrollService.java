cb4526a2e4d91626494257f7b46af23aeb66c180
Mehakjit <mehakjitsingh@gmail.com>
2020-10-19 05:27:04 +0530

    [Mehakjit] Added: Employee Payroll service master to perform IO basic operations

---
+++ src/main/java/com/capg/EmloyeePayrollService.java
@@ -0,0 +1,40 @@
+package com.capg;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmloyeePayrollService {
+	
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmloyeePayrollService() {
+	}
+
+	public EmloyeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	private void readEmployeePayrollData(Scanner scaanerinputsfromconsole) {
+		System.out.print("Enter Employee ID: ");
+		int id = scaanerinputsfromconsole.nextInt();
+		System.out.print("Enter Employee Name: ");
+		String name = scaanerinputsfromconsole.next();
+		System.out.print("Enter Employee Salary: ");
+		double salary = scaanerinputsfromconsole.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	private void writeEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("\nWriting Employee Payroll Data to Console\n" + employeePayrollList);
+	}
+
+	public static void main(String[] args) {
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmloyeePayrollService employeePayrollService = new EmloyeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData(consoleInputReader);
+	}
+}
+
