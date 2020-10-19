6a89bf3282270b4556943477f4d73f46e09fec4b
Mehakjit <mehakjitsingh@gmail.com>
2020-10-19 08:28:31 +0530

    [Mehakjit] Created: EmployeePayrollService to stoe the data into a file

---
+++ src/test/java/com/capg/EmployeePayrollServiceTest.java
@@ -0,0 +1,23 @@
+package com.capg;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.capg.EmloyeePayrollService.IOService;
+
+public class EmployeePayrollServiceTest {
+	@Test
+	public void given3employeewhenWrittenShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfemp = {
+				new EmployeePayrollData(1, "Jeff Bezos", 10000.0),
+				new EmployeePayrollData(2, "Bill Gates", 20000.0),
+				new EmployeePayrollData(3, "Mark Zukerberg", 10000.0),
+		};
+		EmloyeePayrollService employeePayrollService;
+		employeePayrollService = new EmloyeePayrollService(Arrays.asList(arrayOfemp));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		Assert.assertEquals(3, employeePayrollService.countEntries(EmloyeePayrollService.IOService.FILE_IO));
+	}
+}
